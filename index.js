28fb5a305caf9a6f2f313e1481f652c678f2d821
Cristina Murillo <cmurill1@jhu.edu>
2018-07-24 17:32:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes={}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  Object.assign({}, object, {key: value})
-  return
+  var newObject = Object.assign({}, object, {key: value})
+  return newObject 
 }
