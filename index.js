e72e5efa3b873ad37cb9d31c941fffaab880305d
Cristina Murillo <cmurill1@jhu.edu>
2018-07-24 17:45:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return newObject
 }
 
-function 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key]=value;
+  return object
+}
